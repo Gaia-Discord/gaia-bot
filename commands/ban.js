18a9c2388e63dbaccc51de40c5a7c244fd43e3fd
Gaia <59805090+Gaia-Discord@users.noreply.github.com>
2020-03-09 16:16:31 +0100

    Fixed Permissions

--- commands/ban.js
+++ commands/ban.js
@@ -4,6 +4,7 @@ let ft = require('../index.js');
 const fs = module.require('fs');
 
 module.exports.run = async (bot, message, args) => {
+    if(message.member.roles.find(r => r.name === "Gaia Permissions")){
         let member = message.mentions.members.first() || message.guild.members.get(args[0]); //|| message.guild.members.get(args[0]); // Specify the Member
         let reason = args.slice(1).join(" ");
 
@@ -12,6 +13,7 @@ module.exports.run = async (bot, message, args) => {
 
         await message.guild.ban(member);
         return
+    } else {return message.channel.send(ft.gaiaEmbed("You do not have the required permissions to perform this command.", "#7289da"))}
 }
 
 module.exports.help = {
